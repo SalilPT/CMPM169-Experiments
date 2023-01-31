601cd34aa728a265200af746fa44c6790439e7c6
SalilPT <32131426+SalilPT@users.noreply.github.com>
2023-01-31 02:13:14 -0800

    Added code for Experiment 2

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -2,27 +2,36 @@
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+const WIDTH = 1280;
+const HEIGHT = 800;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+const STROKE_WEIGHT = 3;
+const NUM_TRACKING_RECTS = 1280;
+const MAX_TRACKING_RECT_LENGTH = 12;
+const TRACKING_RECT_WIDTH = 2;
 
-// Globals
-let myInstance;
-let canvasContainer;
+let trackingRects = [];
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+class TrackingRect {
+  constructor(x, y, length, angleOffset) {
+    this.x = x;
+    this.y = y;
+    this.length = length;
+    this.angleOffset = angleOffset + HALF_PI; // Add HALF_PI here to account for original rectangle orientation
 
-    myMethod() {
-        // code to run when method is called
-    }
+    this.currAngle = atan2(mouseY - this.y, mouseX - this.x) + this.angleOffset;
+    this.color = "black";
+  }
+
+  display() {
+    stroke(this.color);
+    fill(this.color);
+
+    this.currAngle = atan2(mouseY - this.y, mouseX - this.x) + this.angleOffset;
+    translate(this.x, this.y);
+    rotate(this.currAngle);
+    rect(0, 0, TRACKING_RECT_WIDTH, this.length);
+  }
 }
 
 // setup() function is called once when the program starts
@@ -36,32 +45,31 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    canvas = createCanvas(WIDTH, HEIGHT);
+    background(255);
+    strokeWeight(STROKE_WEIGHT);
+    rectMode(CENTER);
+
+    for (let i = 0; i < NUM_TRACKING_RECTS; i++) {
+        trackingRects.push(new TrackingRect(
+        round(random(WIDTH)),
+        round(random(HEIGHT)),
+        1 + round(random(MAX_TRACKING_RECT_LENGTH)),
+        random(PI/4)
+        ));
+    }
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+function mouseMoved() {
+    clear();
+    for (let trackingRect of trackingRects) {
+      push();
+      trackingRect.display();
+      pop();
+    }
+  }
