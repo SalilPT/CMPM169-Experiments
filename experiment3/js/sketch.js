3a81c5fd97cf129bacf8c44b1ebffc6ef17345b2
SalilPT <32131426+SalilPT@users.noreply.github.com>
2023-01-31 02:18:44 -0800

    Added code for Experiment 3

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -2,27 +2,60 @@
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+const width = 100;
+const height = 100;
+const cellSize = 10;
+let grid;
+let nextGrid;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+const ANT_START_POS = {
+  x: Math.floor(width / 2),
+  y: Math.floor(height / 2)
+};
+const ANT_START_DIRECTION = { // Vector for a cardinal direction
+  x: 0,
+  y: -1
+};
 
-// Globals
-let myInstance;
-let canvasContainer;
+let ant;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+class LangtonsAnt {
+  constructor(startPos, initialDirection) {
+    this.pos = startPos;
+    this.direction = initialDirection;
+  }
 
-    myMethod() {
-        // code to run when method is called
-    }
+  // Outline ant position
+  drawSelf() {
+    noFill();
+    stroke(216, 64, 32);
+    strokeWeight(4);
+    rect(ant.pos.x * cellSize, ant.pos.y * cellSize, cellSize, cellSize);
+    stroke(0);
+    strokeWeight(1);
+  }
+
+  moveForward() {
+    grid[this.pos.x][this.pos.y] = Number(!grid[this.pos.x][this.pos.y]) * 2; // Sets cell value to either 0 or 2
+    this.pos.x = (this.pos.x + this.direction.x) % width;
+    this.pos.x = this.pos.x < 0 ? width - 1 : this.pos.x;
+    this.pos.y = (this.pos.y + this.direction.y) % height;
+    this.pos.y = this.pos.y < 0 ? height - 1 : this.pos.y;
+  }
+
+  turnLeft() {
+    this.direction = { // Multiply direction vector by rotation matrix
+      x: this.direction.y,
+      y: -this.direction.x
+    };
+  }
+
+  turnRight() {
+    this.direction = { // Multiply direction vector by rotation matrix
+      x: -this.direction.y,
+      y: this.direction.x
+    };
+  }
 }
 
 // setup() function is called once when the program starts
@@ -36,32 +69,92 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
-}
+    createCanvas(width * cellSize, height * cellSize);
+    background(0);
+    grid = create2DArray(width, height);
+    nextGrid = create2DArray(width, height);
+    // randomly populate grid
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        grid[x][y] = floor(random(2));
+      }
+    }
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    ant = new LangtonsAnt(ANT_START_POS, ANT_START_DIRECTION);
+  }
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
+  function draw() {
+    // draw current state of grid
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        if (grid[x][y] === 1) { // Live cell
+          fill(255);
+        }
+        else if (grid[x][y] === 2) { // Persistent live cell (created by ant)
+          fill(0, 255, 0);
+        }
+        else {
+          fill(floor(48 * noise(x, y, frameCount / 15))); // Dead Cell
+        }
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+    }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+    ant.drawSelf();
+
+    doAntUpdate();
+    doGameOfLifeUpdate();
+
+    // update grid
+    grid = nextGrid;
+    nextGrid = create2DArray(width, height);
+  }
+
+  function countNeighbors(x, y) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        let col = (x + i + width) % width;
+        let row = (y + j + height) % height;
+        sum += grid[col][row] ? 1 : 0;
+      }
+    }
+    sum -= grid[x][y];
+    return sum;
+  }
+
+  function create2DArray(cols, rows) {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = new Array(rows);
+    }
+    return arr;
+  }
+
+  function doAntUpdate() {
+    grid[ant.pos.x][ant.pos.y] >= 1 ? ant.turnRight() : ant.turnLeft();
+    ant.moveForward();
+  }
+
+  function doGameOfLifeUpdate() {
+    // update next grid
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        // Don't count neighbors if it's a persistent cell
+        if (grid[x][y] === 2) {
+          nextGrid[x][y] = grid[x][y];
+          continue;
+        }
+
+        let neighbors = countNeighbors(x, y);
+        if (grid[x][y] === 1 && (neighbors < 2 || neighbors > 3)) {
+          nextGrid[x][y] = 0;
+        } else if (grid[x][y] === 0 && neighbors === 3) {
+          nextGrid[x][y] = 1;
+        } else {
+          nextGrid[x][y] = grid[x][y];
+        }
+      }
+    }
+  }
