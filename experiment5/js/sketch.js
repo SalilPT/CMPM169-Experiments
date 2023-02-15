2a0c6082b6757f4af220aa4053805511dfe88c13
SalilPT <32131426+SalilPT@users.noreply.github.com>
2023-02-14 20:00:36 -0800

    Added code for Experiment 5

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -2,26 +2,130 @@
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
 // Globals
-let myInstance;
-let canvasContainer;
+var particles = [];
+let planarShockwaves = [];
+let explosionClouds = [];
+let BASE_BACKGROUND_COLOR;
+let FLASH_BACKGROUND_COLOR;
+let currBackgroundColorLerpValue = 0;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+class particle{
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.ded = false;
+        this.vel = {
+            x: random(-1, 1),
+            y: random(-1, 1),
+            z: random(-1, 1)
+        };
+        this.total = 0.5/(sqrt((this.vel.x*this.vel.x)+(this.vel.y*this.vel.y)+(this.vel.z*this.vel.z))+random(-0.9, -0.3));
+        this.vel = {
+            x: this.vel.x*this.total,
+            y: this.vel.y*this.total,
+            z: this.vel.z*this.total
+        };
+        this.color = random(50, 255);
+        this.age = 0;
+    }
+    update(){
+        this.age += random(0.2, 2);
+        if(this.ded === false && 150<this.age){
+            this.ded = true;
+        }
+        this.vel = {
+            x: this.vel.x*random(0.99, 1.001),
+            y: this.vel.y*random(0.99, 1.001),
+            z: this.vel.z*random(0.99, 1.001)
+        };
+        this.x += this.vel.x;
+        this.y += this.vel.y;
+        this.z += this.vel.z;
+        if(this.color<240){
+            let greenColorValueOffset = 50 * (noise(frameCount / 20) - 0.5);
+            fill(255, this.color + greenColorValueOffset, 0);
+        }
+        else{
+            fill(this.color);
+        }
+        translate(this.x, this.y, this.z);
+        sphere(10, 8, 4);
+        translate(-this.x, -this.y, -this.z);
+    }
+}
+
+class CenteredPlanarShockwave {
+    constructor() {
+        this.MAX_RADIUS = 1000;
+        this.currentRadius = 0;
+        this.growthRate = 10;
+        this.xRot = 60 * (random() - 0.5); // Random x rotation from -30 degrees to 30 degrees
+        this.yRot = 60 * (random() - 0.5);
+
+        this.dead = false;
     }
 
-    myMethod() {
-        // code to run when method is called
+    update() {
+        this.currentRadius += this.growthRate;
+        if (this.currentRadius > this.MAX_RADIUS) {
+            this.dead = true;
+            return;
+        }
+
+        fill(240);
+        push();
+        rotateX(this.xRot);
+        rotateY(this.yRot);
+        torus(this.currentRadius, 4);
+        pop();
+    }
+}
+
+class ExplosionCloud {
+    constructor() {
+        this.xScale = 1 + random(1,3);
+        this.yScale = 1 + random(1,3);
+        this.zScale = 1 + random(1,3);
+        this.currentBaseRadius = 0;
+        this.growthRate = 4;
+
+        this.xRot = random(360);
+        this.yRot = random(360);
+        this.zRot = random(360);
+
+        this.xRotInc = 10 * (random() - 0.5);
+        this.yRotInc = 10 * (random() - 0.5);
+        this.zRotInc = 10 * (random() - 0.5);
+
+        this.currentAlpha = 255;
+
+        this.dead = false;
+    }
+
+    update() {
+        fill(216, this.currentAlpha);
+
+        // Stop updating this ellipsoid if its color alpha value is less than or equal to 0
+        this.currentAlpha -= 8;
+        if (this.currentAlpha <= 0) {
+            this.dead = true;
+            return;
+        }
+
+        this.xRot += this.xRotInc;
+        this.yRot += this.yRotInc;
+        this.zRot += this.zRotInc;
+
+        this.currentBaseRadius += this.growthRate;
+
+        push();
+        rotateX(this.xRot);
+        rotateY(this.yRot);
+        rotateZ(this.zRot);
+        ellipsoid(this.xScale * this.currentBaseRadius, this.yScale * this.currentBaseRadius, this.zScale * this.currentBaseRadius);
+        pop();
     }
 }
 
@@ -29,39 +133,76 @@ class MyClass {
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+    BASE_BACKGROUND_COLOR = color(51);
+    FLASH_BACKGROUND_COLOR = color(255, 236, 196); // Very light orange
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    angleMode(DEGREES);
+    background(FLASH_BACKGROUND_COLOR);
+    for(var i = 0; i<1000; i++){
+        particles.push(new particle);
+    }
+    noStroke();
+
+    planarShockwaves.push(new CenteredPlanarShockwave());
+    explosionClouds.push(new ExplosionCloud());
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    rotateX(-mouseY / 2);
+    rotateY(mouseX / 2);
+    offset = 0;
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    currBackgroundColorLerpValue = min(currBackgroundColorLerpValue + 0.08, 1);
+    background(lerpColor(FLASH_BACKGROUND_COLOR, BASE_BACKGROUND_COLOR, currBackgroundColorLerpValue));
+
+    for(var i = 0; i<particles.length; i++){
+        particles[i].update();
+    }
+    for(var i = 0; i<particles.length; i++){
+        if(particles[i+offset].ded){
+            particles.splice(i+offset, 1);
+            offset--;
+            i++;
+        }
+    }
+
+    // Planar shockwaves
+    let aliveShockwaves = [];
+    for (let shockwave of planarShockwaves) {
+        shockwave.update();
+        if (shockwave.dead) {
+            continue;
+        }
+        aliveShockwaves.push(shockwave);
+    }
+    planarShockwaves = aliveShockwaves;
+
+    // Explosion clouds
+    let aliveExplosionClouds = [];
+    for (let cloud of explosionClouds) {
+        cloud.update();
+        if (cloud.dead) {
+            continue;
+        }
+        aliveExplosionClouds.push(cloud);
+    }
+    explosionClouds = aliveExplosionClouds;
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function mouseReleased() {
+    for(var i = 0; i<500; i++){
+        particles.push(new particle);
+    }
+    planarShockwaves.push(new CenteredPlanarShockwave());
+    explosionClouds.push(new ExplosionCloud());
+    background(FLASH_BACKGROUND_COLOR);
+    currBackgroundColorLerpValue = 0;
 }
